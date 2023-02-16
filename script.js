71c7103c72fcf0d656787bba7a4cb0930b66f74e
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-16 09:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+const songName = "Enoo ratrulosthayi";
+const videoUrl = `https://www.youtube.com/watch?v=9esI5PpvY2Q`;
+const favoriteSongDiv = document.getElementById("favorite-song");
+favoriteSongDiv.innerHTML = `My current favorite song is <a href="${videoUrl}" target="_blank">${songName}</a>.`;
